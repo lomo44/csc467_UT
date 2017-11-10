b13ba9695be179b90b110a7fe9a364325fcffb98
Zhuang Li <johnnn.li@mail.utoronto.ca>
2017-11-10 17:21:10 -0500

    - Added function header for symbol table

--- Starter3/symbol.h
+++ Starter3/symbol.h
@@ -1,5 +1,29 @@
 #ifndef _SYMBOL_H
 #define _SYMBOL_H
 
+#include <string>
+#include <vector>
+#include "ast.h"
+
+struct cpSymbolAttribute{
+    std::string m_sIdentifierName;
+    int m_iType;
+    int m_iAttribute;
+};
+
+typedef std::vector<cpSymbolAttribute> cpSymbolTable;
+
+
+struct cpSymbolTableNode{
+    cpSymbolTableNode* m_pParentScope = NULL;
+    int m_iNumOfChildScopes = -1;
+    cpSymbolTableNode** m_pChileScopes = NULL;
+    cpSymbolTable m_vSymbolTable;
+};
+
+cpSymbolTableNode* constructSymbolTable(cpBaseNode* in_pNode);
+
+
+
 #endif
 
